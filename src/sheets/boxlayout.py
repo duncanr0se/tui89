6d46be76c5dcf66d74698161aedf6a1eba0ea547
Duncan Rose <drose@equalexperts.com>
2022-02-20 12:28:34 +0000

    Move shared box layout functionality to superclass

--- src/sheets/boxlayout.py
+++ src/sheets/boxlayout.py
@@ -8,7 +8,37 @@ from sheets.spacereq import ySpaceReqDesired
 # A layout that arranges its children in columns
 class BoxLayout(Sheet):
 
-    # list of relative widths or absolute values
+    # List of relative widths or absolute values.
+
+    # List defines ratios for children. If not set, all children are
+    # allocated equal space. If provided, defines both the layout for
+    # the children and the number of children.
+
+    # Possible values:
+
+    #   - [] :: no sizing information given, all children allocated
+    #           equal major space.
+    #   - [N1, N2, ..., Nn] :: each N defines a ratio to be allocated
+    #           to the corresponding child. For example [2, 3, 5]
+    #           defines a layout where the first child has 2/10ths of
+    #           available major space, second has 3/10ths, and 3rd
+    #           has 1/2. Calculated by summing elements then calculating
+    #           percentages
+    #   - [(N1, "char"), (N2, "char"), ...] :: each N defines a number
+    #           of units to allocate to the corresponding child.
+    #   - [(N1, "ratio"), (N2, "ratio"), ...] :: each N defines a
+    #           ratio of the overall space; same as [N1, N2, ...]
+    #   - [(N1, "%"), (N2, "%"), ...] :: define a specific percentage
+    #           of the available space to allocate to corresponding
+    #           child.
+    #
+    # The different forms above may be mixed; for example, [(N1, "char"),
+    # (N2, "%"), (N3, "char"), (N4, "ratio"), (N5, "ratio")]. In this
+    # case the explicit ("char") sizes are allocated first; any "percentage"
+    # values are converted to ratios, and then all ratios are calculated
+    # using the space remaining after fixed sizes have been set.
+
+    # FIXME: if anything needs unit tests, it's probably the above!
     _portions = None
 
     def __init__(self, portions):
@@ -17,6 +47,33 @@ class BoxLayout(Sheet):
 
     def add_child(self, child):
         super().add_child(child)
+
+    def major_size_component(self, sheet):
+        pass
+
+    # perhaps this should be the thing that calls compose-space
+    # followed by allocate-space? With compose-space on kids in turn
+    # calling layout? Probably best to follow same pattern as DUIM if
+    # only for consistency in my own stuff...
+    #
+    # maybe allocate_space should call layout() if the region changes?
+    def layout(self):
+        offset = 0
+        for child in self._children:
+            child.move_to((offset, 0))
+            offset += self.major_size_component(child)
+            child.layout()
+
+    def render(self):
+        if not self._region:
+            raise RuntimeError("render invoked before space allocation")
+        # layouts always clear their background; concrete widgets may
+        # choose to do so if they need to but most will fill their
+        # region anyway and they can rely on empty space being the
+        # default background colour.
+        self.clear((0, 0), self._region)
+        for child in self._children:
+            child.render()
 
 
 class HorizontalLayout(BoxLayout):
@@ -57,19 +114,12 @@ class HorizontalLayout(BoxLayout):
                 surplus -= 1
             child.allocate_space((callocx, height))
 
-    def layout(self):
-        offset = 0
-        for child in self._children:
-            child.move_to((offset, 0))
-            offset += child.width()
-            child.layout()
+    def major_size_component(self, sheet):
+        return sheet.width()
 
-    def render(self):
-        if not self._region:
-            raise RuntimeError("render invoked before space allocation")
-        self.clear((0, 0), self._region)
-        for child in self._children:
-            child.render()
+
+    def minor_size_component(self, sheet):
+        return sheet.height()
 
 
 # A layout that arranges its children in rows
@@ -114,26 +164,8 @@ class VerticalLayout(BoxLayout):
                 surplus -= 1
             child.allocate_space((width, callocy))
 
-    # perhaps this should be the thing that calls compose-space
-    # followed by allocate-space? With compose-space on kids in turn
-    # calling layout? Probably best to follow same pattern as DUIM if
-    # only for consistency in my own stuff...
-    #
-    # maybe allocate_space should call layout() if the region changes?
-    def layout(self):
-        offset = 0
-        for child in self._children:
-            child.move_to((0, offset))
-            offset += child.height()
-            child.layout()
+    def major_size_component(self, sheet):
+        return sheet.height()
 
-    def render(self):
-        if not self._region:
-            raise RuntimeError("render invoked before space allocation")
-        # layouts always clear their background; concrete widgets may
-        # choose to do so if they need to but most will fill their
-        # region anyway and they can rely on empty space being the
-        # default background colour.
-        self.clear((0, 0), self._region)
-        for child in self._children:
-            child.render()
+    def minor_size_component(self, sheet):
+        return sheet.width()
