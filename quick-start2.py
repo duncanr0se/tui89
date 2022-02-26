e8143329eab14a16f1eb5208a4c466d029bb95aa
Duncan Rose <drose@equalexperts.com>
2022-02-26 18:26:50 +0000

    Repo tidying for push to GitHub

--- quick-start2.py
+++
@@ -1,9 +0,0 @@
-from asciimatics.screen import Screen
-from time import sleep
-
-def demo(screen):
-    screen.print_at("Hello, world!", 0, 0)
-    screen.refresh()
-    sleep(10)
-
-Screen.wrapper(demo)
