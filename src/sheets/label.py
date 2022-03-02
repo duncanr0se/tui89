6096d857571ea6a9291d5d45b71cb7e138739b91
Duncan Rose <drose@equalexperts.com>
2022-03-02 17:54:55 +0000

    Label refactorings
    Documentation, functional decomposition

--- src/sheets/label.py
+++ src/sheets/label.py
@@ -24,10 +24,19 @@ from sheets.spacereq import SpaceReq, FILL
 from dcs.ink import Pen
 
 class Label(Sheet):
+    """Line of text.
 
-    _label_text = None
+    Display a line of text. Text may be left, right, or center aligned
+    in the region allocated to the Label.
 
-    def __init__(self, label_text="", default_pen=None, pen=None, align=None):
+    If the label is shrunk the text displayed will be truncated and
+    elipses appended to indicate that part of the label is omitted.
+    """
+    def __init__(self,
+                 label_text="",
+                 default_pen=None,
+                 pen=None,
+                 align=None):
         super().__init__(default_pen=default_pen, pen=pen)
         self._label_text = label_text
         valid_aligns = { None, "left", "right", "center", "centre" }
@@ -37,7 +46,10 @@ class Label(Sheet):
 
     def __repr__(self):
         (width, height) = self._region
-        return "Label({}x{}, '{}')".format(width, height, self._label_text)
+        tx = self._transform._dx
+        ty = self._transform._dy
+        return "Label({}x{}@{},{}: '{}')".format(width, height, tx, ty,
+                                                 self._label_text)
 
     def pen(self):
         if self._pen is None:
@@ -47,28 +59,44 @@ class Label(Sheet):
     def add_child(self):
         raise RuntimeError("children not allowed")
 
-    # drawing / redisplay
     def render(self):
-        # todo: label alignment
         pen = self.pen()
-        display_text = self._label_text
-        # truncate so it's possible to see at least 3+ label chars +
-        # "...". If text has 3 or fewer characters and doesn't fit in
-        # the available space, then that's just tough. Text will be
-        # clipped.
-        if len(display_text)>self.width() and len(display_text)>3:
-            display_text = display_text[:self.width()-3]
-            display_text += "..."
-        if self._align is None or self._align == "left":
-            coord = (0, 0)
-        elif self._align == "right":
-            coord = (self.width()-len(display_text), 0)
-        else: #self._align == "center" or self._align == "centre":
-            coord = ((self.width()-len(display_text)) // 2, 0)
+        # fixme: paint background, decide where label should be if height>1
+        display_text = self.truncate_text_to_width(self._label_text, self.width())
+        coord = (self.line_offset(self._align, display_text, self.width()), 0)
         self.display_at(coord, display_text, pen)
 
-    # layout
+    def truncate_text_to_width(self, display_text, width):
+        """Truncate text to fit widget.
+
+        Truncate so it's possible to see at least 3+ label chars +
+        "...". If text has 6 or fewer characters and doesn't fit in
+        the available space, then that's just tough. Text will be
+        clipped.
+        """
+        if len(display_text)>width and len(display_text)>6:
+            display_text = display_text[:width-3]
+            display_text += "..."
+        return display_text
+
+    def line_offset(self, align, text, width):
+        """Calculate offset of text for given alignment.
+
+        Alignment must be one of:
+           + "left",
+           + "right",
+           + {"center", "center"}
+        """
+        if align is None or align == "left":
+            return 0
+        elif align == "right":
+            return width-len(text)
+        else:
+            # align == "center" or "centre"
+            return (width-len(text)) // 2
+
     def compose_space(self):
         # Prefer enough room for the label. Can take as much room as offered.
-        # todo: check if this needs a smaller minimum (3 chars + "..."?)
-        return SpaceReq(len(self._label_text), len(self._label_text), FILL, 1, 1, FILL)
+        # Won't shrink below 3 chars from label + "..." (= 6 chars)
+        label_min = min(len(self._label_text), 6)
+        return SpaceReq(label_min, len(self._label_text), FILL, 1, 1, FILL)
